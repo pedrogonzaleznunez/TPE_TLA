e97858b5874f542d05135dc2e2115bc7382e7cb4
fspivak <fespivak@itba.edu.ar>
2026-05-07 14:49:19 -0300

    refactor: update BisonActions declarations for new AST node types

--- src/main/c/frontend/syntactic-analysis/BisonActions.h
+++ src/main/c/frontend/syntactic-analysis/BisonActions.h
@@ -10,16 +10,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/** Initialize module's internal state. */
 ModuleDestructor initializeBisonActionsModule(CompilerState * compilerState);
 
 /* Hardware */
-Pin DigitalPinSemanticAction(int number);
-Pin AnalogPinSemanticAction(int number);
-HardwareDecl * SimpleHardwareDeclSemanticAction(HwDeviceType device, char * name, Pin pin);
-HardwareDecl * UltrasonicHardwareDeclSemanticAction(char * name, Pin trig, Pin echo);
-HardwareDecl * LcdHardwareDeclSemanticAction(char * name);
-HardwareDecl * AppendHardwareDeclSemanticAction(HardwareDecl * list, HardwareDecl * decl);
-HardwareBlock * HardwareBlockSemanticAction(HardwareDecl * decls);
+ComponentType ComponentTypeSemanticAction(TokenLabel token);
+PinSpec * IntegerPinSpecSemanticAction(int pin);
+PinSpec * IdentifierPinSpecSemanticAction(char * pinIdentifier);
+PinSpec * NamedListPinSpecSemanticAction(PinSpecEntry * entries);
+PinSpec * IntListPinSpecSemanticAction(PinSpecEntry * entries);
+PinSpecEntry * NewPinSpecEntryListSemanticAction(PinSpecEntry * entry);
+PinSpecEntry * AppendPinSpecEntryListSemanticAction(PinSpecEntry * list, PinSpecEntry * entry);
+PinSpecEntry * NamedPinEntrySemanticAction(char * name, int value);
+PinSpecEntry * IntPinEntrySemanticAction(int value);
+HardwareDecl * HardwareDeclSemanticAction(ComponentType ct, char * identifier, PinSpec * pinSpec);
+HardwareDeclList * NewHardwareDeclListSemanticAction(HardwareDecl * decl);
+HardwareDeclList * AppendHardwareDeclListSemanticAction(HardwareDeclList * list, HardwareDecl * decl);
+HardwareBlock * HardwareBlockSemanticAction(HardwareDeclList * decls);
 
 /* Routine */
 RoutineBlock * RoutineBlockSemanticAction(StmtList * stmts);
@@ -29,24 +36,26 @@ StmtList * AppendStmtSemanticAction(StmtList * list, Stmt * stmt);
 Stmt * CallStmtSemanticAction(char * object, char * method, ArgList * args);
 Stmt * VarStmtSemanticAction(char * name, Expr * value);
 Stmt * IfStmtSemanticAction(Expr * cond, StmtList * thenBranch, StmtList * elseBranch);
-Stmt * RepeatEveryStmtSemanticAction(StmtList * body, int intervalMs);
-Stmt * WaitStmtSemanticAction(int delayMs);
+Stmt * RepeatEveryStmtSemanticAction(char * timeLiteral, StmtList * body);
+Stmt * RepeatTimesStmtSemanticAction(int count, StmtList * body);
+Stmt * WaitStmtSemanticAction(char * timeLiteral);
 Stmt * ForRangeStmtSemanticAction(char * varName, Expr * from, Expr * to, StmtList * body);
 
-/* Expressions */
+/* Arguments */
 ArgList * SingleArgSemanticAction(Expr * expr);
 ArgList * AppendArgSemanticAction(ArgList * list, Expr * expr);
+
+/* Expressions */
 Expr * BinaryExprSemanticAction(Expr * left, Operator op, Expr * right);
 Expr * UnaryExprSemanticAction(Operator op, Expr * operand);
-Expr * IdentifierExprSemanticAction(char * name);
 Expr * MemberExprSemanticAction(char * object, char * member);
 Expr * CallExprSemanticAction(char * object, char * method, ArgList * args);
+Expr * IdentifierExprSemanticAction(char * name);
 Expr * IntegerExprSemanticAction(int value);
 Expr * FloatExprSemanticAction(double value);
 Expr * StringExprSemanticAction(char * value);
 Expr * BoolExprSemanticAction(bool value);
-Expr * TimeExprSemanticAction(int timeMs);
-Expr * AnalogPinExprSemanticAction(int pin);
+Expr * TimeExprSemanticAction(char * timeLiteral);
 
 /* Program */
 Program * ProgramSemanticAction(HardwareBlock * hardware, RoutineBlock * routine);
